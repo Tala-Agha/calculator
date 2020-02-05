ab78f58945031c07ebaacf8497560c3d560cf46e
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-02-05 22:18:56 +0200

    Simple Calc

--- calculator.py
+++ calculator.py
@@ -1,10 +1,34 @@
 
 def main():
-	#write your code here
-	pass
-	
+    #write your code here
+    num1 = input("Enter the first number:  ")
+    num2 = input("Enter the second number:  ")
+    oper = input("Choose the operation (+, -, /, *):  ")
+
+    if (num1 and num2).isdigit():
+        num1 = int(num1)
+        num2 = int(num2)
+        if oper == '+':
+            print('{} + {} = '.format(num1, num2))
+            print(num1 + num2)
+        elif oper == '-':
+            print('{} - {} = '.format(num1, num2))
+            print(num1 - num2)
+        elif oper == '*':
+            print('{} * {} = '.format(num1, num2))
+            print(num1 * num2)
+        elif oper == '/':
+            print('{} / {} = '.format(num1, num2))
+            print(num1 / num2)
+        else:
+            print("Invalid operator")
+    else:
+        print("The numbers are invalid")
+
+    pass
+
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
